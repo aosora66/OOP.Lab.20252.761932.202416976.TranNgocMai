1b4ccc5c12b3565afeebb35f665c5faf8d9c05df
aosora66 <tnm662006@gmail.com>
2026-06-02 23:49:36 +0700

    Force remove unnecessary folders, keep only Lab05

--- Lab01/2.2.3/HelloNameDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class HelloNameDialog {
-    static void main(String[] args) {
-        String result;
-        result = JOptionPane.showInputDialog("Please enter your name: ");
-        JOptionPane.showMessageDialog(null, "Hi " + result + " !");
-    }
-}
